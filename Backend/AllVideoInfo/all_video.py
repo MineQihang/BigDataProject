326847c9c98dd8ba9d07b545bca0fa2f551f59c0
Geaming_local <13982596716@163.com>
2022-11-28 21:52:14 +0800

    增加整体视频相关后端接口

--- Backend/AllVideoInfo/all_video.py
+++ Backend/AllVideoInfo/all_video.py
@@ -17,10 +17,24 @@ async def all_video_info(num:int = Form(20)):
     :return list
     '''
     data = {}
-
     # 播放量
     data['play_data'] = query_overall_situation()
-    data['words_count'] = query_words_count(num)
+    data['words_count'] = query_words_count(num) # 词云使用
+    # 饼状图占比
+    rate_data = {}
+    for x in data['play_data']:
+        if x['fq']+'-'+x['cate'] not in rate_data.keys():
+            rate_data[x['fq']+'-'+x['cate']] = 0
+        rate_data[x['fq']+'-'+x['cate']] += x['num']
+    # 折线图data,每个月份各个分区视频发布量
+    line_data = {}
+    for x in data['play_data']:
+        if x['month'] not in line_data.keys():
+            line_data[x['month']] = {}
+        line_data[x['month']][x['fq']+'-'+x['cate']] = x['num']
+
+    data['rate_data'] = rate_data
+    data['line_data'] = line_data
 
     return successResponse(detail="视频基本信息返回成功", data = data) if data != None else failResponse(detail="视频基本信息返回失败")
 
@@ -48,25 +62,27 @@ async def tags_count_change(tag:str=Form("")):
     return successResponse(detail="视频基本信息返回成功", data = data) if data != None else failResponse(detail="视频基本信息返回失败")
 
 @router.post('/tags-count-in-month')
-async def tags_count_in_month(month:int=Form(None), num=10):
+async def tags_count_in_month(month:int=Form(None), num=Form(10)):
     '''
     :param month 不指定就是返回所有月份的数据, 指定了就是那个月的排名前num的tag
     :param num 要看前多少个
     :return {1: [(tag, count), ...], 2:[], ...}
     '''
     data = {}
-    data['tags_count_in_month'] = query_tag_count_change(month, num)
+    data['tags_count_in_month'] = query_tags_count_in_month(month, num)
 
     return successResponse(detail="视频基本信息返回成功", data = data) if data != None else failResponse(detail="视频基本信息返回失败")
 
 @router.post('/tags-relation')
-async def tags_relation(tags = Form):
+async def tags_relation(tags:str = Form()):
     '''
     根据tags返回关联度关系
-    :param tags list 比如["知识分享官", "打卡挑战", "学习" ...]
+    :param tags list 比如 知识分享官,打卡挑战,学习
     :return 三元组 [(index_1, index_2, relation_rate), ...]
     '''
     data = {}
+    tags = tags.split(",")
+    # print(tags)
     data['tags_relation'] = query_tags_relation(tags)
 
     return successResponse(detail="视频基本信息返回成功", data = data) if data != None else failResponse(detail="视频基本信息返回失败")
